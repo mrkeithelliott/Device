1dc107dda1e30fe718f381ae0143c68afc1c6850
Keith Elliott <keith@moven.com>
2016-01-22 16:15:51 -0500

    Turn project into a Framework. Share the scheme for Carthage

---
+++ DeviceLib/DeviceLib.h
@@ -0,0 +1,19 @@
+//
+//  DeviceLib.h
+//  DeviceLib
+//
+//  Created by Keith Elliott on 1/22/16.
+//  Copyright © 2016 Ekhoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DeviceLib.
+FOUNDATION_EXPORT double DeviceLibVersionNumber;
+
+//! Project version string for DeviceLib.
+FOUNDATION_EXPORT const unsigned char DeviceLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceLib/PublicHeader.h>
+
+
--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import DeviceLib
 
 class ViewController: UIViewController {
 
